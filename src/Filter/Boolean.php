718ba058701cafceea2f08c899f025e0d263e772
hslater <hamish.slater@hallowelt.com>
2024-12-06 12:50:28 +0100

    Improve bool support
    Normalise bool in Boolean Filter
    Add bool support in List Filter
    
    ERM39443

--- src/Filter/Boolean.php
+++ src/Filter/Boolean.php
@@ -17,6 +17,12 @@ class Boolean extends Filter {
 		$fieldValue = $dataSet->get( $this->getField() );
 		$filterValue = $this->getValue();
 
+		// Allow specific strings
+		$fieldValue = $this->normaliseBoolean( $fieldValue );
+		if ( $fieldValue === null ) {
+			return false;
+		}
+
 		if ( $this->getComparison() === static::COMPARISON_EQUALS_BOOL ) {
 			return $filterValue === $fieldValue;
 		}
@@ -28,6 +34,32 @@ class Boolean extends Filter {
 			case self::COMPARISON_NOT_EQUALS:
 				return $fieldValue != $filterValue;
 		}
+
 		return false;
+	}
+
+	/**
+	 * Converts specific strings to boolean values
+	 * Returns null if the value is invalid
+	 *
+	 * @param mixed $value The value to normalise
+	 * @return bool|null Boolean value or null if invalid
+	 */
+	private function normaliseBoolean( $value ) {
+		if ( is_bool( $value ) ) {
+			return $value;
+		}
+
+		if ( is_string( $value ) ) {
+			$lower = strtolower( $value );
+			if ( in_array( $lower, [ 'true', 'yes' ], true ) ) {
+				return true;
+			}
+			if ( in_array( $lower, [ 'false', 'no' ], true ) ) {
+				return false;
+			}
+		}
+
+		return null;
 	}
 }
