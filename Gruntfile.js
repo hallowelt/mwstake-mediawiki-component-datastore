c6c962de6eb69c1c9efa3a5e5c5427649370c9c2
hslater <hamish.slater@hallowelt.com>
2025-10-30 18:07:21 +0700

    Fix CI

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = function ( grunt ) {
+	grunt.loadNpmTasks( 'grunt-eslint' );
+	grunt.loadNpmTasks( 'grunt-stylelint' );
+
+	grunt.initConfig( {
+		eslint: {
+			options: {
+				cache: true,
+				fix: grunt.option( 'fix' )
+			},
+			all: '.'
+		},
+		stylelint: {
+			options: {
+				cache: true
+			},
+			all: [
+				'**/*.{css,less}',
+				'!node_modules/**',
+				'!vendor/**'
+			]
+		}
+	} );
+
+	grunt.registerTask( 'test', [ 'eslint', 'stylelint' ] );
+	grunt.registerTask( 'default', 'test' );
+};
